143a0ceddcfbd71e4b681b7eb24e5038d64c7995
tranvietquoc <tranvietquoc5@gmail.com>
2022-08-20 17:06:43 +0700

    bt

---
+++ ss13_thuattoansapxep/InsertionSortSetup.java
@@ -0,0 +1,32 @@
+package ss13_thuattoansapxep;
+
+public class InsertionSortSetup {
+    public static void insertionSort(int array[]) {
+        int length = array.length;
+        for (int j = 1; j < length; j++) {
+            int key = array[j];
+            int i = j - 1;
+            while ((i > -1) && (array[i] > key)) {
+                array[i + 1] = array[i];
+                i--;
+            }
+            array[i + 1] = key;
+        }
+    }
+
+    public static void main(String a[]) {
+        int[] arr1 = {5, 8, 4, 12, 43, 14, 67, 22};
+        System.out.println("Before Insertion Sort");
+        for (int i : arr1) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        insertionSort(arr1);//sorting array using insertion sort
+
+        System.out.println("After Insertion Sort");
+        for (int i : arr1) {
+            System.out.print(i + " ");
+        }
+    }
+}
